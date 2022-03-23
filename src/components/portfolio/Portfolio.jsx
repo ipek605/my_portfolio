0a7edfd3ec30bc15f376794fc7abbd0bd818d722
ipek <ipeksoltanov@hotmail.com>
2022-03-23 20:28:32 +0200

    portfolio created

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,86 @@
+import React, { useState, useEffect } from "react";
 import "./portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
 
 function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    { id: "featured", title: "Featured" },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
   return (
     <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item, index) => (
+          <PortfolioList
+            key={index}
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
 
+        {data.map((d) => (
+           <a href={d.href} className="portfolio-link" target="_blank" rel="noreferrer" key={d.id}>
+           <div className="item">
+             <img src={d.img} alt="images" />
+             <h3>{d.title}</h3>
+           </div>
+         </a>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
+
 
 export default Portfolio
