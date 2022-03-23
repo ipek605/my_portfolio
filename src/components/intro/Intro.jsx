3d15dfe8721be7854c09252be5eff2107ab60b61
ipek <ipeksoltanov@hotmail.com>
2022-03-23 12:12:19 +0200

    intro finished

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,40 @@
+import React, { useEffect, useRef } from "react";
 import "./intro.scss";
+import { init } from "ityped";
 
 function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      backDelay: 1500,
+      backSpeed:60,
+      showCursor:true,
+      strings:["Developer", "Graphic Designer", "Photographer"],
+    })
+  }, []);
   return (
     <div className="intro" id="intro">
-
+      <div className="left">
+        <div className="imgContainer">
+          <img src="images/image.png" alt="photograph" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Ipek Soltanov</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img className="img" src="images/arrow.png" alt="arrowimage" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
+
 
 export default Intro
