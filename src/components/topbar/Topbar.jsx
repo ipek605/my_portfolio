cc55991b77d00c7da26dc0cfcafa7bee8667570a
ipek <ipeksoltanov@hotmail.com>
2022-03-23 05:04:15 +0200

    some css added for navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,20 +1,23 @@
 import "./topbar.scss";
 import { FaEnvelope } from "react-icons/fa";
 
+
 function Topbar() {
   return (
     <div className="topbar">
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            ipek
+            <span>ipek</span>{" "}
           </a>
           <div className="itemContainer">
             <FaEnvelope className="icon" />
             <span>isoltanov605@gmail.com</span>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+ 
+        </div>
       </div>
     </div>
   );
